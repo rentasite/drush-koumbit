dfafa10b3e559742ff09a096c7e74825ac64c106
anarcat <anarcat@koumbit.org>
2009-09-14 11:50:31 +0200

    fix hook documentation and add a bit of explanations and cross references

--- drush.api.php
+++ drush.api.php
@@ -3,7 +3,41 @@
 
 /**
  * @file
- * Documentation for Drush.
+ * Documentation of the Drush API.
+ *
+ * All drush commands are invoked in a specific order, using
+ * drush-made hooks, very similar to the Drupal hook system.
+ *
+ * For any command named "hook", the following hooks are called, in
+ * order:
+ *
+ * 1. drush_hook_validate()
+ * 2. drush_pre_hook()
+ * 3. drush_hook()
+ * 4. drush_post_hook()
+ *
+ * If any of those fails, the rollback mechanism is called. It will
+ * call, in reverse, all _rollback hooks. So if, for example, the
+ * drush_post_hook() call fails, the following hooks will be called:
+ *
+ * 1. drush_post_hook_rollback()
+ * 2. drush_hook_rollback()
+ * 3. drush_pre_hook_rollback()
+ * 4. drush_hook_validate_rollback()
+ *
+ * Before any command is called, drush_hook_init() is also called.
+ *
+ * @see includes/command.inc
+ *
+ * @see drush_hook_init()
+ * @see drush_hook_validate()
+ * @see drush_pre_hook()
+ * @see drush_hook()
+ * @see drush_post_hook()
+ * @see drush_post_hook_rollback()
+ * @see drush_hook_rollback()
+ * @see drush_pre_hook_rollback()
+ * @see drush_hook_validate_rollback()
  */
 
 /**
@@ -22,9 +56,11 @@ function hook_drush_init() {
  * Run before a specific command executes. 
  * 
  * Logging an error stops command execution, and the rollback function (if any)
- * for each hook implementation is invoked - hook_COMMAND_validate_rollback().
+ * for each hook implementation is invoked
+ *
+ * @see drush_hook_validate_rollback()
  */
-function hook_COMMAND_validate() {
+function drush_hook_validate() {
 
 }
 
@@ -32,9 +68,30 @@ function hook_COMMAND_validate() {
  * Run before a specific command executes. Logging an error stops command execution.
  *
  * Logging an error stops command execution, and the rollback function (if any)
- * for each hook implementation is invoked - hook_pre_COMMAND_rollback().
+ * for each hook implementation is invoked, in addition to the
+ * validate rollback
+ *
+ * @see drush_pre_hook_rollback()
+ * @see drush_hook_validate_rollback()
  */
-function hook_pre_COMMAND() {
+function drush_pre_hook() {
+
+}
+
+/**
+ * Implementation of the actual drush command
+ *
+ * This is where most of the stuff should happen
+ *
+ * Logging an error stops command execution, and the rollback function (if any)
+ * for each hook implementation is invoked, in addition to pre and
+ * validate rollbacks.
+ *
+ * @see drush_hook_rollback()
+ * @see drush_pre_hook_rollback()
+ * @see drush_hook_validate_rollback()
+ */
+function drush_hook() {
 
 }
 
@@ -42,9 +99,15 @@ function hook_pre_COMMAND() {
  * Run after a specific command executes. Logging an error stops command execution.
  * 
  * Logging an error stops command execution, and the rollback function (if any)
- * for each hook implementation is invoked - hook_post_COMMAND_rollback().
+ * for each hook implementation is invoked, in addition to pre, normal
+ * and validate rollbacks.
+ *
+ * @see drush_post_hook_rollback()
+ * @see drush_hook_rollback()
+ * @see drush_pre_hook_rollback()
+ * @see drush_hook_validate_rollback()
  */
-function hook_post_COMMAND() {
+function drush_post_hook() {
 
 }
 
@@ -68,10 +131,6 @@ function hook_drush_pm_post_install($project, $release, $destination) {
 function hook_pm_post_update($release_name, $release_candidate_version, $project_parent_path) {
 
 }
-
-
-
-
 
 /**
  * @} End of "addtogroup hooks".
